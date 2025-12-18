6a27ec621375bec1fc8fdf51c68de88229621c53
S.P.Chandra Sai <spchandrasai820@gmail.com>
2025-12-18 23:53:36 +0530

    added score, highScore and time

--- Snake Game/script.js
+++ Snake Game/script.js
@@ -3,31 +3,35 @@ const startButton = document.querySelector('.btn-start');
 const modal = document.querySelector('.modal');
 const startGameModal = document.querySelector('.start-game');
 const gameOverModal = document.querySelector('.game-over');
+const restartButton = document.querySelector('.btn-restart');
+const highScoreElement = document.querySelector("#high-score");
+const scoreElement = document.querySelector("#score");
+const timeElement = document.querySelector("#time");
 
 const blockHeight = 50;
 const blockWidth = 50;
 
+let highScore = localStorage.getItem("highScore") || 0;
+let score = 0;
+let time = `00:00`;
+
+highScoreElement.innerText = highScore;
+
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
 let intervalId = null;
+let timerIntervalId = null;
 let food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) };
 
 const blocks = [];
-const snake = [{ x: 1, y: 3 }];
+let snake = [{ x: 1, y: 3 }];
 let direction = 'right'
-
-// for (let i = 0; i < rows * cols; i++){
-//     const block = document.createElement('div');
-//     block.classList.add('block');
-//     board.appendChild(block);
-// }
 
 for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
         const block = document.createElement('div');
         block.classList.add('block');
         board.appendChild(block);
-        block.innerText = `${row}-${col}`;
         blocks[`${row}-${col}`] = block;
     }
 }
@@ -50,12 +54,18 @@ function render() {
         head = { x: snake[0].x - 1, y: snake[0].y };
     }
 
+    // wall collision logic
     if (head.x < 0 || head.y < 0 || head.x >= rows || head.y >= cols) {
-        alert("Game Over");
         clearInterval(intervalId);
+
+        modal.style.display = "flex";
+        startGameModal.style.display = "none";
+        gameOverModal.style.display = "flex";
+
         return;
     }
 
+    // food consume logic
     if (head.x == food.x && head.y == food.y) {
         blocks[`${food.x}-${food.y}`].classList.remove("food");
         food = {
@@ -63,6 +73,15 @@ function render() {
         };
         blocks[`${food.x}-${food.y}`].classList.add("food");
         snake.unshift(head);
+
+        score += 10;
+        scoreElement.innerText = score;
+
+        if (score > highScore) {
+            highScore = score;
+            highScoreElement.innerText = highScore;
+            localStorage.setItem("highScore", highScore.toString());
+        }
     }
 
     snake.forEach(segment => {
@@ -76,17 +95,46 @@ function render() {
     });
 }
 
-// intervalId = setInterval(() => {
-//     render();
-// }, 400);
-
 startButton.addEventListener("click", () => {
     modal.style.display = "none";
     intervalId = setInterval(() => { render() }, 300);
+    timerIntervalId=setInterval(()=>{
+        let [min,sec]=time.split(":").map(Number);
+
+        if(sec==59){
+            min+=1;
+            sec=0;
+        }
+        else{
+            sec+=1;
+        }
+
+        time=`${min.toString().padStart(2,'0')}:${sec.toString().padStart(2,'0')}`;
+        timeElement.innerText=time;
+    }, 1000);
 });
+
+restartButton.addEventListener("click", restartGame);
 
 function restartGame() {
 
+    blocks[`${food.x}-${food.y}`].classList.remove("food");
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.remove('fill');
+    });
+    score = 0;
+    scoreElement.innerText = score;
+
+    time = `00:00`;
+    timeElement.innerText = time;
+
+    highScoreElement.innerText = highScore;
+
+    modal.style.display = "none";
+    direction = "down";
+    snake = [{ x: 1, y: 3 }];
+    food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) };
+    intervalId = setInterval(() => { render() }, 300);
 }
 
 addEventListener("keydown", (event) => {
