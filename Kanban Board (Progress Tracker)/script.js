86a6cd7ff7d71a3c6dae23da4fbbd784b29a11de
S.P.Chandra Sai <spchandrasai820@gmail.com>
2026-01-14 01:43:39 +0530

    Added new project-Kanban Board

---
+++ Kanban Board (Progress Tracker)/script.js
@@ -0,0 +1,102 @@
+let taskData={};
+
+const todo = document.querySelector("#todo");
+const progress = document.querySelector("#progress");
+const done = document.querySelector("#done");
+const columns = [todo, progress, done];
+let dragElement = null;
+
+const tasks = document.querySelectorAll(".task");
+
+tasks.forEach(task => {
+    task.addEventListener("drag", (e) => {
+        // console.log("dragging", e);
+        dragElement = task;
+    })
+})
+
+function addDragEventOnColumn(column) {
+    column.addEventListener("dragenter", (e) => {
+        e.preventDefault();
+        column.classList.add("hover-over");
+    })
+
+    column.addEventListener("dragleave", (e) => {
+        e.preventDefault();
+        column.classList.remove("hover-over");
+    })
+
+    column.addEventListener("dragover", (e) => {
+        e.preventDefault();
+    })
+
+    column.addEventListener("drop", (e) => {
+        e.preventDefault();
+
+        console.log("Dropped", dragElement, column);
+
+        column.appendChild(dragElement);
+        column.classList.remove("hover-over");
+
+        columns.forEach(col => {
+            const tasks = col.querySelectorAll(".task");
+            const count = col.querySelector(".right");
+
+            count.innerText = tasks.length;
+        })
+    })
+}
+
+addDragEventOnColumn(todo);
+addDragEventOnColumn(progress);
+addDragEventOnColumn(done);
+
+const toggleModalButton = document.querySelector("#toggle-modal");
+const modalBg = document.querySelector(".modal .bg");
+const modal = document.querySelector(".modal");
+const addTaskButton = document.querySelector("#add-new-task");
+
+toggleModalButton.addEventListener("click", () => {
+    modal.classList.toggle("active");
+})
+
+modalBg.addEventListener("click", () => {
+    modal.classList.remove("active");
+})
+
+addTaskButton.addEventListener("click", () => {
+    const taskTitle = document.querySelector("#task-title-input").value;
+    const taskDesc = document.querySelector("#task-desc-input").value;
+
+    const div = document.createElement("div");
+
+    div.classList.add("task");
+    div.setAttribute("draggable", "true");
+    div.innerHTML = `
+        <h2>${taskTitle}</h2>
+        <p>${taskDesc}</p>
+        <button>Delete</button>
+    `
+    todo.appendChild(div);
+
+    columns.forEach(col => {
+        const tasks = col.querySelectorAll(".task");
+        const count = col.querySelector(".right");
+
+        taskData[col.id]=Array.from(tasks).map(t=>{
+            return{
+                title: t.querySelector("h2").innerText,
+                desc: t.querySelector("p").innerText
+            }
+        })
+
+        localStorage.setItem("tasks", JSON.stringify(taskData));
+        count.innerText = tasks.length;
+    })
+
+    div.addEventListener("drag", (e) => {
+        dragElement = div;
+    })
+
+    modal.classList.remove("active");
+})
